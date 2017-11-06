409f1234ba0df12a9cc94b4a4a7d1bc1d2c285b1
lonelyarcher <xumeng@gmail.com>
2017-11-05 20:01:26 -0500

    ori todo

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+let nextTodoId = 0;
+export const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    id: (nextTodoId++).toString(),
+    text,
+  };
+};
+
+export const setVisibilityFilter = (filter) => ({
+    type: 'SET_VISIBILITY_FILTER',
+    filter,
+  });
+
+export const toggleTodo = (id) => ({
+  type: 'TOGGLE_TODO',
+    id,
+  });
